7b7db33d03674640c72573246c606708a8d4372a
ShangHungWan <sun@taiyou.tw>
2024-01-08 23:56:25 +0800

    feat: run for n rounds and get average execution time by argv

--- quicksort.cpp
+++ quicksort.cpp
@@ -7,31 +7,37 @@ using namespace std;
 using namespace chrono;
 typedef long long ll;
 
-#define SWAP(x, y)  ll tmp = (x); \
-                    (x) = (y); \
-                    (y) = (tmp);
+#define SWAP(x, y) \
+    ll tmp = (x);  \
+    (x) = (y);     \
+    (y) = (tmp);
 
 // Partition 函數
-ll partition(vector<ll>& arr, ll low, ll high) {
-    ll pivot = arr[high];  // 選擇最右邊的元素作為 pivot
+ll partition(vector<ll> &arr, ll low, ll high)
+{
+    ll pivot = arr[high]; // 選擇最右邊的元素作為 pivot
 
     // 小於 pivot 的元素將被放在左邊
     // 大於 pivot 的元素將被放在右邊
     ll i = low - 1;
 
-    for (ll j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
+    for (ll j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
             i++;
             SWAP(arr[i], arr[j]);
         }
     }
-    SWAP(arr[i + 1], arr[high]);  // 將 pivot 放在正確的位置
-    return i + 1; // 回傳 pivot 的位置
+    SWAP(arr[i + 1], arr[high]); // 將 pivot 放在正確的位置
+    return i + 1;                // 回傳 pivot 的位置
 }
 
 // Quick Sort 函數
-void quickSort(vector<ll>& arr, ll low, ll high) {
-    if (low < high) {
+void quickSort(vector<ll> &arr, ll low, ll high)
+{
+    if (low < high)
+    {
         // 選擇 pivot 並找到 partition 的位置
         ll pi = partition(arr, low, high);
 
@@ -41,47 +47,74 @@ void quickSort(vector<ll>& arr, ll low, ll high) {
     }
 }
 
-void read_data(vector<ll>& arr) {
+bool read_data(vector<ll> &arr)
+{
     ifstream inFile("random_numbers.bin", ios::binary | ios::in);
 
-    if (inFile.is_open()) {
-        ll value;
-        while (inFile.read(reinterpret_cast<char*>(&value), sizeof(ll))) {
-            arr.push_back(value);
-        }
-
-        inFile.close();
-        cout << "Number read from binary file successfully\n";
-    } else {
-        cerr << "Can't open random_numbers.bin\n";
+    if (!inFile.is_open())
+    {
+        return false;
     }
+
+    ll value;
+    while (inFile.read(reinterpret_cast<char *>(&value), sizeof(ll)))
+    {
+        arr.push_back(value);
+    }
+
+    inFile.close();
+
+    return true;
 }
 
 // If correct, print nothing. Otherwise, print error index and its value
-void validate(vector<ll>& arr) {
+void validate(vector<ll> &arr)
+{
     ll n = arr.size();
-    for (ll i = 0; i < n-1; i++) {
-        if (arr[i] > arr[i+1]) {
-            cout << "error at " << i << " " << arr[i] << " " << arr[i+1] << endl;
+    for (ll i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            cout << "error at " << i << " " << arr[i] << " " << arr[i + 1] << endl;
         }
     }
 }
 
-int main() {
+int main(int argc, char **argv)
+{
+    int rounds = 1;
+    double total_time = 0;
+    if (argc == 2)
+    {
+        rounds = atoi(argv[1]);
+    }
+    cout << "Run for [" << rounds << "] rounds" << endl;
 
     vector<ll> arr;
-    read_data(arr);
+    if (!read_data(arr))
+    {
+        cerr << "Can't read data\n";
+        return 1;
+    }
     ll n = arr.size();
     cout << "Load count:" << n << endl;
 
-    auto start_time = high_resolution_clock::now();
-    quickSort(arr, 0, n - 1);
-    auto end_time = high_resolution_clock::now();
+    for (int i = 0; i < rounds; i++)
+    {
+        vector<ll> temp = vector<ll>(arr);
+        auto start_time = high_resolution_clock::now();
 
-    auto duration = duration_cast<milliseconds>(end_time - start_time);
-    cout << "Execution time: " << duration.count() << " ms" << endl;
+        quickSort(temp, 0, n - 1);
 
-    validate(arr);
+        auto end_time = high_resolution_clock::now();
+        auto duration = duration_cast<milliseconds>(end_time - start_time);
+        cout << "[Round " << i << "] Execution time: " << duration.count() << " ms" << endl;
+        total_time += duration.count();
+
+        validate(temp);
+    }
+
+    cout << "Average time: " << total_time / rounds << " ms" << endl;
 
     return 0;
 }
